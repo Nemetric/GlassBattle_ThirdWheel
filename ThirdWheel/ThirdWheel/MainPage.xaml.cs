32e7731cccafc8957521a4ba9136039a20fd7cb2
adamhahnsolo <ahahn@live.com>
2017-01-12 13:23:06 -0600

    NOt broken bruh

--- ThirdWheel/ThirdWheel/MainPage.xaml.cs
+++ ThirdWheel/ThirdWheel/MainPage.xaml.cs
@@ -20,6 +20,9 @@ namespace ThirdWheel
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    
+    public 
+
     public sealed partial class MainPage : Page
     {
         public MainPage()
