00046e315389e2a172770c971f37f46b9fabb913
adamhahnsolo <ahahn@live.com>
2017-01-20 12:40:55 -0600

    Added a timer Bruh

---
+++ ThirdWheel/ThirdWheel/Devices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdWheel
+{
+    [DataContract]
+    public class Devices
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Color { get; set; }
+        [DataMember]
+        public string FriendlyName { get; set; }
+
+
+
+    }
+}
--- ThirdWheel/ThirdWheel/MainPage.xaml.cs
+++ ThirdWheel/ThirdWheel/MainPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -21,7 +24,7 @@ namespace ThirdWheel
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    
+
 
     public sealed partial class MainPage : Page
     {
@@ -30,14 +33,38 @@ namespace ThirdWheel
             this.InitializeComponent();
         }
         //a = opacity
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-            string hexin = "#FFFFFF";
-            string r = hexin.Substring(1, 2);
-            string g = hexin.Substring(3, 2);
-            string b = hexin.Substring(5, 2);
 
-            rect.Fill = new SolidColorBrush(Color.FromArgb(255, Convert.ToByte(r, 16), Convert.ToByte(g, 16), Convert.ToByte(b, 16)));
+        private async void UpdateColor(object sender, object e)
+        {
+            try
+            {
+                HttpClient httpclient = new HttpClient();
+
+                var response = await httpclient.GetAsync("http://localhost:58592/display/whatnext/1");
+
+                var ResponseText = await response.Content.ReadAsStringAsync();
+
+                var asdf = JsonConvert.DeserializeObject<Devices>(ResponseText);
+
+                string hexin = asdf.Color;
+                string r = hexin.Substring(1, 2);
+                string g = hexin.Substring(3, 2);
+                string b = hexin.Substring(5, 2);
+
+                rect.Fill = new SolidColorBrush(Color.FromArgb(255, Convert.ToByte(r, 16), Convert.ToByte(g, 16), Convert.ToByte(b, 16)));
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void rect_Loaded(object sender, RoutedEventArgs e)
+        {
+            var timer = new DispatcherTimer();
+            timer.Tick += UpdateColor;
+            timer.Interval = new TimeSpan(0, 0, 0, 1);
+            timer.Start();
         }
     }
 }
